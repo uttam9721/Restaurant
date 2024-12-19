e16d84c98e565678059d0f84535ba3d06982322d
Uttam Kumar <uttammaurya377@gmail.com>
2024-12-19 15:18:20 +0530

    cart done

--- Frontend/src/context/storeContext.jsx
+++ Frontend/src/context/storeContext.jsx
@@ -69,9 +69,17 @@ const StoreContextProvider = (props) => {
     });
   };
 
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
+  const getTotalCartAmount=()=>{
+    let totalAmount = 0;
+    for(const item in cartItems){
+      if(cartItems[item]>0){
+        // totalAmount += food_list[item].price * cartItems[item];
+        let itemInfo = food_list.find((product)=>product._id===item)
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+    }
+    return totalAmount;
+  }
 
   const contextValue = {
     food_list,
@@ -79,6 +87,7 @@ const StoreContextProvider = (props) => {
     setCartItems,
     addToCart,
     removeFromCart,
+    getTotalCartAmount
   };
 
   return (
--- Frontend/src/pages/Cart/Cart.jsx
+++ Frontend/src/pages/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import { StoreContext } from '../../context/StoreContext';
 import { assets, food_list } from '../../assets/assets';
 const Card = () => {
 
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart,getTotalCartAmount } = useContext(StoreContext);
 
   return (
 
@@ -39,23 +39,38 @@ const Card = () => {
          }
         })}
       </div>
-      <div className="cart-botton">
-        <h2>Cart Total</h2>
-        <div>
-          <div className="cart-total-details">
-            <p>Subtotal</p>
-          </div>
-          <div className="cart-total-details">
-            <p>Delivery Fee</p>
-            <p>{2}</p>
-          </div>
-          <div className="cart-total-details">
-            <b>Total</b>
-            <b>{0}</b>
-          </div>
-        </div>
-          <button>Proceed To Checkout</button>
+      <div className="cart-bottom"> 
+  <div className="cart-total">
+    <h2>Cart Totals</h2>
+    <div>
+      <div className="cart-total-details">
+        <p>Subtotal</p>
+        <p>₹ {getTotalCartAmount()}</p>
       </div>
+      <hr />
+      <div className="cart-total-details">
+        <p>Delivery Fee</p>
+        <p>₹ { 30}</p>
+      </div>
+      <hr />
+      <div className="cart-total-details">
+        <b>Total</b>
+        <b>₹ {getTotalCartAmount()+30}</b>
+      </div>
+    </div>
+    <button>PROCEED TO CHECKOUT</button>
+  </div>
+  <div className="cart-promocode">
+    <div>
+      <p>If you have Promo Code, Enter it here</p>
+      <div className="cart-promocode-input">
+        <input type="text" placeholder="Enter Promo Code" />
+        <button>APPLY</button>
+      </div>
+    </div>
+  </div>
+</div>
+
     </div>
   )
 }
