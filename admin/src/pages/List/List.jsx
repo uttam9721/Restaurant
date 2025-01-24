99ba81657e43006903f4bc1a42a95e7e8e593259
Uttam Kumar <uttammaurya377@gmail.com>
2025-01-24 23:01:46 +0530

    item part done

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,35 +1,113 @@
-// import React, { useEffect, useState } from 'react'
-// import './List.css'
-// import {toast} from 'react-toastify'
-// import axios from 'axios'
-// import { assets } from '../../assets/assets'
+import React, { useEffect, useState } from 'react'
+import './List.css'
+import {toast} from 'react-toastify'
+import axios from 'axios'
+import { assets } from '../../assets/assets'
+const List = ({url}) => {
+
+    // const url = "http://localhost:4000";
+  const [list,setList]= useState([]);
+
+  const fetchList = async() =>{
+
+    const response = await axios.get(`${url}/api/food/list`);
+    // console.log(response.data)
+    if(response.data.success){
+      setList(response.data.data)
+    }
+    else{
+      toast.error("Error")
+    }
+  }
+  useEffect(()=>{
+    fetchList();
+
+  },[])
+
+  // remove cross functionality
+  const removeFood=async(foodId)=>{
+    const response = await axios.post(`${url}/api/food/remove`,{id:foodId});
+    await fetchList();
+    if(response.data.success){
+      toast.success(response.data.message);
+    }else{
+      toast.error("Error");
+    }
+
+  }
+
+  return (
+    <div className='list add flex-col'>
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item,index)=>{
+          return(
+            <div key={index}  className="list-table-format">
+              <img src={`${url}/images/`+item.image} alt="img" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>₹{item.price}</p>
+              <p onClick={()=>removeFood(item._id)} className='cursor'>X</p>  
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default List
+
+
+
+// import React, { useEffect, useState } from 'react';
+// import './List.css';
+// import { toast } from 'react-toastify';
+// import axios from 'axios';
+
 // const List = () => {
+//   const url = "http://localhost:4000";
+//   const [list, setList] = useState([]);
 
-//     const url = "http://localhost:4000";
-//   const [list,setList]= useState([]);
-
-//   const fetchList = async() =>{
-
-//     const response = await axios.get(`${url}/api/food/list`);
-//     // console.log(response.data)
-//     if(response.data.success){
-//       setList(response.data.data)
+//   const fetchList = async () => {
+//     try {
+//       const response = await axios.get(`${url}/api/food/list`);
+//       if (response.data.success) {
+//         setList(response.data.data);
+//       } else {
+//         toast.error("Error fetching food list");
+//       }
+//     } catch (error) {
+//       console.error("Error fetching food list:", error);
+//       toast.error("An error occurred while fetching food list");
 //     }
-//     else{
-//       toast.error("Error")
-//     }
-//   }
-//   useEffect(()=>{
+//   };
+
+//   useEffect(() => {
 //     fetchList();
+//   }, []);
 
-//   },[])
-
-//   // remove cross functionality
-//   const removeFood=async()=>{
-//     const response = await axios.post(`${url}/api/food/remove`,{id:foodId});
-//     await fetchList();
-
-//   }
+//   const removeFood = async (foodId) => {
+//     try {
+//       const response = await axios.post(`${url}/api/food/remove`, { id: foodId });
+//       if (response.data.success) {
+//         toast.success("Food removed successfully");
+//         await fetchList();
+//       } else {
+//         toast.error("Failed to remove food");
+//       }
+//     } catch (error) {
+//       console.error("Error removing food:", error);
+//       toast.error("An error occurred while removing food");
+//     }
+//   };
 
 //   return (
 //     <div className='list add flex-col'>
@@ -42,92 +120,18 @@
 //           <b>Price</b>
 //           <b>Action</b>
 //         </div>
-//         {list.map((item,index)=>{
-//           return(
-//             <div key={index}  className="list-table-format">
-//               <img src={`${url}/images/`+item.image} alt="img" />
-//               <p>{item.name}</p>
-//               <p>{item.category}</p>
-//               <p>₹{item.price}</p>
-//               <p onClick={()=>removeFood(item._id)} className='cursor'>X</p>  
-//             </div>
-//           )
-//         })}
+//         {list.map((item, index) => (
+//           <div key={index} className="list-table-format">
+//             <img src={`${url}/images/` + item.image} alt="img" />
+//             <p>{item.name}</p>
+//             <p>{item.category}</p>
+//             <p>₹{item.price}</p>
+//             <p onClick={() => removeFood(item._id)} className='cursor'>X</p>
+//           </div>
+//         ))}
 //       </div>
 //     </div>
-//   )
-// }
+//   );
+// };
 
-// export default List
-
-
-
-import React, { useEffect, useState } from 'react';
-import './List.css';
-import { toast } from 'react-toastify';
-import axios from 'axios';
-
-const List = () => {
-  const url = "http://localhost:4000";
-  const [list, setList] = useState([]);
-
-  const fetchList = async () => {
-    try {
-      const response = await axios.get(`${url}/api/food/list`);
-      if (response.data.success) {
-        setList(response.data.data);
-      } else {
-        toast.error("Error fetching food list");
-      }
-    } catch (error) {
-      console.error("Error fetching food list:", error);
-      toast.error("An error occurred while fetching food list");
-    }
-  };
-
-  useEffect(() => {
-    fetchList();
-  }, []);
-
-  const removeFood = async (foodId) => {
-    try {
-      const response = await axios.post(`${url}/api/food/remove`, { id: foodId });
-      if (response.data.success) {
-        toast.success("Food removed successfully");
-        await fetchList();
-      } else {
-        toast.error("Failed to remove food");
-      }
-    } catch (error) {
-      console.error("Error removing food:", error);
-      toast.error("An error occurred while removing food");
-    }
-  };
-
-  return (
-    <div className='list add flex-col'>
-      <p>All Foods List</p>
-      <div className="list-table">
-        <div className="list-table-format title">
-          <b>Image</b>
-          {/* <b>Image</b> */}
-          <b>Name</b>
-          <b>Category</b>
-          <b>Price</b>
-          <b>Action</b>
-        </div>
-        {list.map((item, index) => (
-          <div key={index} className="list-table-format">
-            <img src={`${url}/images/` + item.image} alt="img" />
-            <p>{item.name}</p>
-            <p>{item.category}</p>
-            <p>₹{item.price}</p>
-            <p onClick={() => removeFood(item._id)} className='cursor'>X</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default List;
+// export default List;
---
+++ backend/controllers/userController.js
@@ -0,0 +1,18 @@
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+
+
+// Login user
+const loginUser = async(req,res) => {
+
+    
+}
+
+
+//  Registeruser
+const registerUser = async(req,res) => {
+
+}
+export {loginUser,registerUser};
