9a1c318056f8952d97eab598358ee977fd52c22d
Uttam Kumar <uttammaurya377@gmail.com>
2025-05-23 04:09:02 +0530

    fixed

--- backend/server.js
+++ backend/server.js
@@ -39,4 +39,5 @@ app.get('/', (req, res) => {
 // Start the server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+    // console.log(`Server running on port ${port}`);
 });
