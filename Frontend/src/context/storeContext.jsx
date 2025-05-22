069df43164223c2375fb734c3e8503e63900bd12
Uttam Kumar <159714121+uttam9721@users.noreply.github.com>
2025-05-23 02:51:03 +0530

    Update storeContext.jsx

--- Frontend/src/context/storeContext.jsx
+++ Frontend/src/context/storeContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://backend-1yrx.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]); // Initialize as an empty array
 
