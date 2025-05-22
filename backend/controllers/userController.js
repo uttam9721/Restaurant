f87e33509ca5db20b12ed2adab0dca25b0840252
Uttam Kumar <uttammaurya377@gmail.com>
2025-05-23 02:47:16 +0530

    done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+// import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import validator from "validator";
 
 // Login User
