867e51ec42c1ed1acf1def9ef15c8ea7b769f226
Uttam Kumar <uttammaurya377@gmail.com>
2025-06-15 20:03:34 +0530

    i am fixing the bug

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 import Verify from './pages/Verify/Verify'
 import MyOrders from './pages/MyOrders/MyOrders';
-// import MyOrders from './pages/MyOrders/MyOrders';
+import MyOrders from './pages/MyOrders/MyOrders';
 
 const App = () => {
   const [showLogin,setShowLogin] =useState(false);
